c35e510c8b2c1d5e4efd2e622f410eb78f2ddaef
Vaughan Hilts <vhilts@axonify.com>
2014-10-28 14:13:47 -0400

    Fixed back button logic

--- js/app.js
+++ js/app.js
@@ -1,27 +1,8 @@
-   function loadHome() {   $.mobile.changePage( "index.html", { transition: "slideup", changeHash: false });}
-   function loadBack() {   $.mobile.changePage( "index.html", { transition: "slideup", changeHash: false });}
-
-   function loadColin() {   $.mobile.changePage( "c1.html", { transition: "slide", changeHash: false });}
-   function loadVaughan() {   $.mobile.changePage( "v1.html", { transition: "slide", changeHash: false });}
-   function loadBrandon() {   $.mobile.changePage( "b1.html", { transition: "slide", changeHash: false });}
 
 
-
-
-   function loadV1() {   $.mobile.changePage( "v1.html", { transition: "slideup", changeHash: false });}
-   function loadV2() {   $.mobile.changePage( "v2.html", { transition: "slideup", changeHash: false });}
-   function loadV3() {   $.mobile.changePage( "v3.html", { transition: "slideup", changeHash: false });}
-
-
-   function loadC1() {   $.mobile.changePage( "c1.html", { transition: "slideup", changeHash: false });}
-   function loadC2() {   $.mobile.changePage( "c2.html", { transition: "slideup", changeHash: false });}
-   function loadC3() {   $.mobile.changePage( "c3.html", { transition: "slideup", changeHash: false });}
-
-   function loadB1() {   $.mobile.changePage( "b1.html", { transition: "slideup", changeHash: false });}
-   function loadB2() {   $.mobile.changePage( "b2.html", { transition: "slideup", changeHash: false });}
-   function loadB3() {   $.mobile.changePage( "b3.html", { transition: "slideup", changeHash: false });}
-
-
+  function loadBack(where) {
+      $.mobile.changePage(where + ".html", { transition: "slideup", changeHash: false });
+   }
 
   /*
     * Allows an application to go to the specified page for performing back operations.
