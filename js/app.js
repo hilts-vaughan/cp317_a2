5d27af08c4af335b4fbd6febcba3a3044a4b4781
Vaughan Hilts <vhilts@axonify.com>
2014-10-28 13:42:07 -0400

    Updated pages

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@
    function loadBrandon() {   $.mobile.changePage( "b1.html", { transition: "slide", changeHash: false });}
 
 
+
+
    function loadV1() {   $.mobile.changePage( "v1.html", { transition: "slideup", changeHash: false });}
    function loadV2() {   $.mobile.changePage( "v2.html", { transition: "slideup", changeHash: false });}
    function loadV3() {   $.mobile.changePage( "v3.html", { transition: "slideup", changeHash: false });}
@@ -21,12 +23,31 @@
 
 
 
-  /**
+  /*
+    * Allows an application to go to the specified page for performing back operations.
+  */
+  function loadBackPage() {
+
+  }
+
+
+  /*
+    * Loads the home page of the applications and navigates there.
+  */
+  function loadHome() {
+    $.mobile.changePage( "index.html", { transition: "slideup", changeHash: false });
+  }
+
+  /*
     * Given the name of a person and the tab, loads and changes to the page required.
     * @param {string} person: The name of the person we will be switching to
     * @param {string} tab:    The tab of this persons profile we will be swapping to
   */
-   function loadPageFor(person, tab) {
+   function loadProfilePageFor(person, tab, transition) {
+
+      // If we are given no transition, we will fall back on slideup
+      transition = transition || "slideup";
+
       person = person.toLowerCase()
       tab = tab.toLowerCase();
       fragment = person + "_" + tab;
